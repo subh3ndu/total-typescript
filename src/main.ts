25a9ab54d25d2cddd2969846d4e4eb97b2ac603d
Subhendu  Karmakar <subhendu.karmakar@bipros.com>
2024-02-12 16:36:03 +0530

    3.2 extract union string matching a pattern

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,3 @@
-type Route = `/${string}`;
+type Routes = "/users" | "/users/:id" | "/posts" | "/posts/:id";
 
-export const goToRoute = (route: Route) => {}
-
-// Should Success
-goToRoute("/users");
-goToRoute("/");
-goToRoute("/admin/users");
-
-// Should Error
-
-// @ts-expect-error
-goToRoute('users/1')
-
-// @ts-expect-error
-goToRoute('http://facebook.com')
+type DynamicRoutes = Extract<Routes, `${string}:${string}`>
