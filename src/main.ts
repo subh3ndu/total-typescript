64c4557464e7a7f4c7588686373a077f60c76adf
Subhendu  Karmakar <subhendu.karmakar@bipros.com>
2024-02-12 12:49:26 +0530

    2.6 extract discriminator from a discriminated union

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,15 @@
-export const fakeDataDefaults = {
-  String: "Default String",
-  Int: 1,
-  Float: 1.23,
-  Boolean: true,
-  Id: "abcdko9384982fdacd",
-};
+export type Events =
+  | {
+      type: "click";
+      event: MouseEvent;
+    }
+  | {
+      type: "focus";
+      event: FocusEvent;
+    }
+  | {
+      type: "keydown";
+      event: KeyboardEvent;
+    };
 
-type FakeDataDefaults = typeof fakeDataDefaults;
-
-export type StringTypeObj = Pick<FakeDataDefaults, "String">;
-export type IntTypeObj = Pick<FakeDataDefaults, "Int">;
-export type FloatTypeObj = Pick<FakeDataDefaults, "Float">;
-export type BooleanTypeObj = Pick<FakeDataDefaults, "Boolean">;
-export type IdObj = Pick<FakeDataDefaults, "Id">;
-
-export type StringType = FakeDataDefaults["String"];
-export type IntType = FakeDataDefaults["Int"];
-export type FloatType = FakeDataDefaults["Float"];
-export type BooleanType = FakeDataDefaults["Boolean"];
-export type Id = FakeDataDefaults["Id"];
+export type EventType = Events["type"];
