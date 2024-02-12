3cad9bfae6a9c193dcbd49d30ee9f61bcf9920d6
Subhendu  Karmakar <subhendu.karmakar@bipros.com>
2024-02-12 14:48:00 +0530

    2.8 Create a union from an object's values

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,19 @@
-export const programModeEnumMap = Object.freeze({
+export const programModeEnumMap = {
   GROUP: "group",
   ANNOUNCEMENT: "announcement",
   ONE_ON_ONE: "1on1",
   SELF_DIRECTED: "selfDirected",
   PLANNED_ONE_ON_ONE: "planned1on1",
   PLANNED_SELF_DIRECTED: "plannedSelfDirected",
-});
+} as const;
 
-type ProgramModeEnumMap = typeof programModeEnumMap;
+export type IndivisualProgram = (typeof programModeEnumMap)[
+  | "ONE_ON_ONE"
+  | "SELF_DIRECTED"
+  | "PLANNED_ONE_ON_ONE"
+  | "PLANNED_SELF_DIRECTED"];
 
-export type GroupProgram = ProgramModeEnumMap["GROUP"];
-export type AnnouncementProgram = ProgramModeEnumMap["ANNOUNCEMENT"];
-export type OneOnOneProgram = ProgramModeEnumMap["ONE_ON_ONE"];
-export type SelfDirectedProgram = ProgramModeEnumMap["SELF_DIRECTED"];
-export type PlannedOneOnOneProgram = ProgramModeEnumMap["PLANNED_ONE_ON_ONE"];
-export type PlannedSelfDirectedProgram =
-  ProgramModeEnumMap["PLANNED_SELF_DIRECTED"];
+export type IndivisualProgram2 = (typeof programModeEnumMap)[Exclude<
+  keyof typeof programModeEnumMap,
+  "GROUP" | "ANNOUNCEMENT"
+>];
