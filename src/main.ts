a3ac6ddce3a1d0e3edb672ac4ffaa8baf5fee74c
Subhendu  Karmakar <subhendu.karmakar@bipros.com>
2024-02-12 12:42:50 +0530

    2.5 Extract Object Properties into individual types

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,21 @@
-export type Events =
-  | {
-      type: "click";
-      event: MouseEvent;
-    }
-  | {
-      type: "focus";
-      event: FocusEvent;
-    }
-  | {
-      type: "keydown";
-      event: KeyboardEvent;
-    };
+export const fakeDataDefaults = {
+  String: "Default String",
+  Int: 1,
+  Float: 1.23,
+  Boolean: true,
+  Id: "abcdko9384982fdacd",
+};
 
-export type ClickEvent = Exclude<Events, { type: "keydown" }>;
+type FakeDataDefaults = typeof fakeDataDefaults;
+
+export type StringTypeObj = Pick<FakeDataDefaults, "String">;
+export type IntTypeObj = Pick<FakeDataDefaults, "Int">;
+export type FloatTypeObj = Pick<FakeDataDefaults, "Float">;
+export type BooleanTypeObj = Pick<FakeDataDefaults, "Boolean">;
+export type IdObj = Pick<FakeDataDefaults, "Id">;
+
+export type StringType = FakeDataDefaults["String"];
+export type IntType = FakeDataDefaults["Int"];
+export type FloatType = FakeDataDefaults["Float"];
+export type BooleanType = FakeDataDefaults["Boolean"];
+export type Id = FakeDataDefaults["Id"];
