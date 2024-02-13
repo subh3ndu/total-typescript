f14dcbda0e2354d707ab551e7ec0f547576e2acb
Subhendu  Karmakar <subhendu.karmakar@bipros.com>
2024-02-13 12:25:54 +0530

    5.2 refine conditional logic in a type helper

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,15 @@
-type YouSayGoodbyeAndISayHello<T> = T extends "hello" ? "goodbye" : "hello";
+type EG<T extends string> = Lowercase<T> extends "hello"
+  ? "goodbye"
+  : Lowercase<T> extends "goodbye"
+  ? "hello"
+  : never;
 
-var Example: YouSayGoodbyeAndISayHello<"a">;
+let Example: EG<"a">;
+
+type YouSayGoodbyeAndISayHello<T> = T extends "hello" | "goodbye"
+  ? T extends "hello"
+    ? "goodbye"
+    : "hello"
+  : never;
+
+let Example2: YouSayGoodbyeAndISayHello<"hola">;
